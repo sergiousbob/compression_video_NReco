062597415a1713709f63681b2147d0080f2392ac
Sergei Serov <caesarreg@mail.ru>
2024-11-19 22:42:25 +0500

    Добавьте файлы проекта.

---
+++ compression_video/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿
+namespace compression_video
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnCompress = new System.Windows.Forms.Button();
+            this.resolBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.formatBox = new System.Windows.Forms.ComboBox();
+            this.bntOutput = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.btnBackOut = new System.Windows.Forms.Button();
+            this.btnForwarOut = new System.Windows.Forms.Button();
+            this.codecBox = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Image = ((System.Drawing.Image)(resources.GetObject("btnBack.Image")));
+            this.btnBack.Location = new System.Drawing.Point(40, 112);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(85, 34);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Image = ((System.Drawing.Image)(resources.GetObject("btnForward.Image")));
+            this.btnForward.Location = new System.Drawing.Point(140, 112);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(85, 34);
+            this.btnForward.TabIndex = 0;
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Image = ((System.Drawing.Image)(resources.GetObject("btnOpen.Image")));
+            this.btnOpen.Location = new System.Drawing.Point(544, 19);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(98, 52);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(130, 25);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(367, 26);
+            this.txtPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(37, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source path";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(40, 160);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(627, 395);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // btnCompress
+            // 
+            this.btnCompress.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCompress.Image = ((System.Drawing.Image)(resources.GetObject("btnCompress.Image")));
+            this.btnCompress.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCompress.Location = new System.Drawing.Point(1206, 102);
+            this.btnCompress.Name = "btnCompress";
+            this.btnCompress.Size = new System.Drawing.Size(139, 52);
+            this.btnCompress.TabIndex = 4;
+            this.btnCompress.Text = "        RUN\r\nCOMPRESS";
+            this.btnCompress.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCompress.UseVisualStyleBackColor = true;
+            this.btnCompress.Click += new System.EventHandler(this.btnCompress_Click);
+            // 
+            // resolBox
+            // 
+            this.resolBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resolBox.FormattingEnabled = true;
+            this.resolBox.Items.AddRange(new object[] {
+            "1280×720 (720p)",
+            "854×480 (480p)",
+            "640×360 (360p)",
+            "426×240 (240p)"});
+            this.resolBox.Location = new System.Drawing.Point(268, 56);
+            this.resolBox.Name = "resolBox";
+            this.resolBox.Size = new System.Drawing.Size(228, 28);
+            this.resolBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(32, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(230, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Select compression options";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(197, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Select video format";
+            // 
+            // formatBox
+            // 
+            this.formatBox.FormattingEnabled = true;
+            this.formatBox.Items.AddRange(new object[] {
+            "flv",
+            "mkv",
+            "webm",
+            "vob",
+            "avi",
+            "wmv",
+            "3gp",
+            "mp4"});
+            this.formatBox.Location = new System.Drawing.Point(376, 67);
+            this.formatBox.Name = "formatBox";
+            this.formatBox.Size = new System.Drawing.Size(121, 28);
+            this.formatBox.TabIndex = 8;
+            // 
+            // bntOutput
+            // 
+            this.bntOutput.Image = ((System.Drawing.Image)(resources.GetObject("bntOutput.Image")));
+            this.bntOutput.Location = new System.Drawing.Point(535, 19);
+            this.bntOutput.Name = "bntOutput";
+            this.bntOutput.Size = new System.Drawing.Size(98, 52);
+            this.bntOutput.TabIndex = 10;
+            this.bntOutput.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bntOutput.UseVisualStyleBackColor = true;
+            this.bntOutput.Click += new System.EventHandler(this.bntOutput_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtOutput.Location = new System.Drawing.Point(852, 37);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(369, 26);
+            this.txtOutput.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(736, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Path to save";
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(19, 148);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(644, 395);
+            this.webBrowser2.TabIndex = 13;
+            // 
+            // btnBackOut
+            // 
+            this.btnBackOut.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnBackOut.Image = ((System.Drawing.Image)(resources.GetObject("btnBackOut.Image")));
+            this.btnBackOut.Location = new System.Drawing.Point(19, 108);
+            this.btnBackOut.Name = "btnBackOut";
+            this.btnBackOut.Size = new System.Drawing.Size(85, 34);
+            this.btnBackOut.TabIndex = 14;
+            this.btnBackOut.UseVisualStyleBackColor = false;
+            this.btnBackOut.Click += new System.EventHandler(this.btnBackOut_Click);
+            // 
+            // btnForwarOut
+            // 
+            this.btnForwarOut.Image = ((System.Drawing.Image)(resources.GetObject("btnForwarOut.Image")));
+            this.btnForwarOut.Location = new System.Drawing.Point(110, 108);
+            this.btnForwarOut.Name = "btnForwarOut";
+            this.btnForwarOut.Size = new System.Drawing.Size(85, 34);
+            this.btnForwarOut.TabIndex = 15;
+            this.btnForwarOut.UseVisualStyleBackColor = true;
+            this.btnForwarOut.Click += new System.EventHandler(this.btnForwarOut_Click);
+            // 
+            // codecBox
+            // 
+            this.codecBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.codecBox.FormattingEnabled = true;
+            this.codecBox.Items.AddRange(new object[] {
+            "AVI",
+            "MP4"});
+            this.codecBox.Location = new System.Drawing.Point(375, 95);
+            this.codecBox.Name = "codecBox";
+            this.codecBox.Size = new System.Drawing.Size(121, 28);
+            this.codecBox.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(720, 568);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Total processed";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(806, 568);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(825, 568);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "of";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(844, 568);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(205, 98);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(164, 20);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Select video format";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.formatBox);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Controls.Add(this.txtPath);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(25, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(673, 549);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input Video";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.webBrowser2);
+            this.groupBox2.Controls.Add(this.bntOutput);
+            this.groupBox2.Controls.Add(this.btnForwarOut);
+            this.groupBox2.Controls.Add(this.btnBackOut);
+            this.groupBox2.Controls.Add(this.codecBox);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.resolBox);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(704, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(673, 549);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output Video";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1393, 590);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnCompress);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "VideoConverter by Sergiousbob";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnCompress;
+        private System.Windows.Forms.ComboBox resolBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox formatBox;
+        private System.Windows.Forms.Button bntOutput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.Button btnBackOut;
+        private System.Windows.Forms.Button btnForwarOut;
+        private System.Windows.Forms.ComboBox codecBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ compression_video/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace compression_video
+{
+    
+    public partial class Form1 : Form
+    {
+        int w_video;
+        int h_video;
+        string format_codec;
+        string codec;
+     
+        public Form1()
+        {
+            InitializeComponent();
+            resolBox.SelectedItem = "854×480 (480p)";
+            formatBox.SelectedItem = "avi";
+            codecBox.SelectedItem = "AVI";
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            
+            
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Choose path" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                    webBrowser1.Url = new Uri (fbd.SelectedPath);
+                    txtPath.Text = fbd.SelectedPath;
+
+            }
+        }
+
+        
+
+        void asyncBtn()
+        {
+            if (resolBox.SelectedItem == "1280×720 (720p)") 
+            {
+                w_video = 1280;
+                h_video = 720;
+
+            }
+
+            if (resolBox.SelectedItem == "854×480 (480p)")
+            {
+                w_video = 854;
+                h_video = 480;
+
+            }
+            if (resolBox.SelectedItem == "640×360 (360p)")
+            {
+                w_video = 640;
+                h_video = 360;
+
+            }
+            if (resolBox.SelectedItem == "426×240 (240p)")
+            {
+                w_video = 426;
+                h_video = 240;
+
+            }
+
+
+            if (codecBox.SelectedItem == "AVI") 
+            {
+                format_codec ="avi";
+   
+
+            }
+
+            if (codecBox.SelectedItem == "MP4") 
+            {
+                format_codec = "mp4";
+
+
+            }
+
+
+
+            string directoryPath = $"{txtPath.Text}";
+
+            string[] aviFiles = Directory.GetFiles(directoryPath, $"*.{formatBox.SelectedItem}");
+            int a = 1;
+
+
+            string kolvo = new DirectoryInfo(directoryPath).GetFiles($"*.{formatBox.SelectedItem}").Length.ToString();
+
+            label8.Text = $"{kolvo}";
+
+
+            foreach (string aviFile in aviFiles)
+
+            {
+
+                label6.Text = $"{a}";
+                var setting = new NReco.VideoConverter.ConvertSettings();
+
+                setting.SetVideoFrameSize(w_video, h_video);
+
+      
+               setting.VideoCodec = "mpeg4";
+
+                var converter = new NReco.VideoConverter.FFMpegConverter();
+
+
+                converter.ConvertMedia(aviFile, $"{formatBox.SelectedItem}", $@"{txtOutput.Text}\VideoConvert{a}.{format_codec}", $"{format_codec}", setting);
+    
+
+                a++;
+          
+            }
+            MessageBox.Show("All videos have been processed!!!");
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+                webBrowser1.GoBack();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+                webBrowser1.GoForward();
+        
+        }
+
+        private async void btnCompress_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => asyncBtn());
+
+            label6.Visible = true;
+            label8.Visible = true;
+            label7.Visible = true;
+       
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntOutput_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Choose path" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                    webBrowser2.Url = new Uri(fbd.SelectedPath);
+                txtOutput.Text = fbd.SelectedPath;
+
+            }
+        }
+
+        private void btnBackOut_Click(object sender, EventArgs e)
+        {
+            if (webBrowser2.CanGoBack)
+                webBrowser2.GoBack();
+
+        }
+
+        private void btnForwarOut_Click(object sender, EventArgs e)
+        {
+            if (webBrowser2.CanGoForward)
+                webBrowser2.GoForward();
+
+        }
+    }
+}
